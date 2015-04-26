76a46191c34e56f4fce80093e2dc92b2e6ef2537
Scott Null <scott.null@gmail.com>
2015-04-25 23:05:49 -0400

    Setup and move files to _TEST and _PROD

---
+++ _TEST/api/api_config.php
@@ -0,0 +1,26 @@
+<?php
+
+// Configuration file for api.php
+// local testing at  http://pushchat.local:44447/test/database.php
+// bluehost testing at http://www.altcoinfolio.com/whereru/api/test/database.php
+$config = array(
+	// These are the settings for development mode
+	'development' => array(
+		'db' => array(
+			'host'     => 'localhost',
+			'dbname'   => 'pushchat',
+			'username' => 'pushchat',
+			'password' => 'd]682\#%yI1nb3',
+			),
+		),
+
+	// These are the settings for production mode
+	'production' => array(
+		'db' => array(
+			'host'     => 'localhost',
+			'dbname'   => 'altcoinf_pushwhereru',
+			'username' => 'altcoinf_pushw',
+			'password' => 'd]682\#%yI1nb3',
+			),
+		),
+	);
