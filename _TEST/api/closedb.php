dc56425dfa76d99667fe59db375933c9b084a9d4
Harnk <scott.null@gmail.com>
2026-06-08 22:36:43 -0400

    cleanup

--- _TEST/api/closedb.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysql_connect('localhost', 'altcoinf_pushw', 'd]682\#%yI1nb3');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_close($link);
-?>
